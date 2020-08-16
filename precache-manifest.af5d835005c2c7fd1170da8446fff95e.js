95a8285a16cfe4284ace492d5512967b495f495f
Mustafa Berber <mustafaberber020@gmail.com>
2020-08-16 11:24:22 +0300

    Updates

---
+++ precache-manifest.af5d835005c2c7fd1170da8446fff95e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f100a44f8b7abec91f4abdf2fc164f68",
+    "url": "/TrackerProject/index.html"
+  },
+  {
+    "revision": "ecb2e1ed38330a3c0fb6",
+    "url": "/TrackerProject/static/css/main.b168fad1.chunk.css"
+  },
+  {
+    "revision": "832feae7a2b484411189",
+    "url": "/TrackerProject/static/js/2.f71f510e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TrackerProject/static/js/2.f71f510e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecb2e1ed38330a3c0fb6",
+    "url": "/TrackerProject/static/js/main.3c083bba.chunk.js"
+  },
+  {
+    "revision": "003043c8d326f6ed48fa",
+    "url": "/TrackerProject/static/js/runtime-main.6b737208.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/TrackerProject/static/media/logo.5d5d9eef.svg"
+  }
+]);
